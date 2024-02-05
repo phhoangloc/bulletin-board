effc2f58efa69d7f475896bfc91d015bdec55249
phhoangloc <82487565+phhoangloc@users.noreply.github.com>
2024-02-05 16:53:03 +0900

    0205

--- src/component/header.tsx
+++ src/component/header.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Header = () => {
     return (
         <div className='header'>
-            <Image src={"/img/header_logo.gif"} width={200} height={40} alt='headerlogo' />
+            <Link href={"/"}><Image src={"/img/header_logo.gif"} width={200} height={40} alt='headerlogo' /></Link>
         </div>
     )
 }
