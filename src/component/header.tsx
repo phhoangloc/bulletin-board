42c16366408ef9bc0372c3845b82a47ed9cae104
phhoangloc <82487565+phhoangloc@users.noreply.github.com>
2024-02-05 16:45:52 +0900

    0205

--- src/component/header.tsx
+++ src/component/header.tsx
@@ -1,12 +1,10 @@
 import Image from 'next/image'
 import React from 'react'
 
-type Props = {}
-
-const Header = (props: Props) => {
+const Header = () => {
     return (
         <div className='header'>
-            <Image src={"/img/header_logo.gif"} width={50} height={40} alt='headerlogo' />
+            <Image src={"/img/header_logo.gif"} width={200} height={40} alt='headerlogo' />
         </div>
     )
 }
