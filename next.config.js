fb0ccb4addda6d6ccea74a31d1800080db3314cd
phhoangloc <82487565+phhoangloc@users.noreply.github.com>
2024-03-08 12:39:00 +0900

    0308

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
         // MONGODB_URL: "mongodb://h-loc:6pS3qvpeUQxuubKi@ac-5djtpn4-shard-00-00.qs9wtoh.mongodb.net:27017,ac-5djtpn4-shard-00-01.qs9wtoh.mongodb.net:27017,ac-5djtpn4-shard-00-02.qs9wtoh.mongodb.net:27017/bulletin?ssl=true&replicaSet=atlas-d75adp-shard-0&authSource=admin&retryWrites=true&w=majority",
         HOMEPAGE_URL: "https://bulletin-board-one.vercel.app/",
         SOCKET_URL: "https://be-socket-mu.vercel.app/",
+        SOCKET_URL_: "http://localhost:4000/",
         HOMEPAGE_URL_: "http://localhost:3000/"
     }
 }
